cddf6a9fac21a2b676c11951a9f83c940f7c249d
Simple-Logz <jobsforjohn10@gmail.com>
2026-08-31 18:59:56 -0400

    style: load embossed validation controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import{StrictMode}from'react';
 import{createRoot}from'react-dom/client';
 import'./index.css';
+import'./validation-metrics.css';
 
 // Block pinch/rotate gestures on touch devices.
 if(typeof window!=='undefined'){
